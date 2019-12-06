ea5c1c97e2d408082ec9cafce3c335cebea1bda8
Eddie Salce <salce048@gmail.com>
2019-12-06 11:02:16 -0500

    Initial Commit

---
+++ Radar_Geofence_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Radar_Geofence_App
+//
+//  Created by Eddie Salce on 12/6/19.
+//  Copyright © 2019 Eddie Salce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
